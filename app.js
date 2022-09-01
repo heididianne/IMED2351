62984d1c9257c76e6536f8e382c2625a88553b20
heididianne <heidilemus@att.net>
2022-09-01 15:51:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-console.log('Hey this is Heidi');
+let life = 100; //This my life bar NUMBER
+
+life = life - 10;
+
+var name = "Heidi"; // This is the name displayed in the game STRING 
+
+
+let x = 20; //GLOBAL
+
+{
+    let x = 10; //BLOCK
+}
+
+
+const pet = {type:"dog", breed:"labrador", color:"black"};
+
+pet.color = "chocolate";
+
+pet.owner = "Me";
+
+
+
+
+console.log(name);
+console.log(life);
+console.log(pet);
 document.write("This class will be fun!")
